3c10b1ac6ae86096470009b0a27a1f926299ed0c
Pedro Vitor Nunes Silva <pedrovitornunes89@gmail.com>
2026-01-13 12:35:40 -0300

    Criando teste de UserRepository

--- src/main/java/ProjetoNelio/dto/UserDtoRequest.java
+++ src/main/java/ProjetoNelio/dto/UserDtoRequest.java
@@ -15,6 +15,13 @@ public class UserDtoRequest {
     @NotBlank
     private String password;
 
+    public UserDtoRequest(String name, String email, String phone, String password) {
+        this.name = name;
+        this.email = email;
+        this.phone = phone;
+        this.password = password;
+    }
+
     public String getName() {
         return name;
     }
