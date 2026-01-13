0626facbaa2c9307862d7bb3fa7fad87cd2b56fd
Pedro Vitor Nunes Silva <pedrovitornunes89@gmail.com>
2026-01-13 15:31:06 -0300

    Colocando dependencia do spring secuirty no projeto

--- src/main/java/E_Commerce_Spring/ECommerceSpringApplication.java
+++ src/main/java/E_Commerce_Spring/ECommerceSpringApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ProjetoNelioApplication {
+public class ECommerceSpringApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ProjetoNelioApplication.class, args);
+		SpringApplication.run(ECommerceSpringApplication.class, args);
 	}
 
 }
